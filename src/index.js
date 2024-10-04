c78e6f807f30e3347df1153699f1e6288c3c7b4e
AgusPaez <agus.123.paez@gmail.com>
2024-10-03 21:42:15 -0300

    changue logic to numbers

--- src/index.js
+++ src/index.js
@@ -66,24 +66,21 @@ app.post("/upload", upload.single("pdf"), async (req, res) => {
       }
 
       // Verificar si la línea tiene un producto y extraer cantidad y precio
-      const productMatch = row.match(/^(.+?)\s+([\d,.]+)\s+(\d+)$/);
+      const productMatch = row.match(/^(\d+)\s+(.+?)\s+X(\d+)U\s+([\d,.]+)$/);
       if (productMatch) {
-        const descripcion = productMatch[1].trim(); // Descripción del artículo (Descripción)
-        const totalPriceStr = productMatch[2].trim(); // Precio total como string
-        const codigo = productMatch[3].trim(); // Código de artículo (Art)
-
-        // Extraer la cantidad de unidades (número antes de "X")
-        const quantityMatch = row.match(/X(\d+)U/);
-        const cantidad = quantityMatch ? parseInt(quantityMatch[1]) : 1; // Por defecto es 1 si no se encuentra
+        const codigo = productMatch[1].trim(); // Código de artículo (Art)
+        const descripcion = productMatch[2].trim(); // Descripción del artículo (Descripción)
+        const cantidad = parseInt(productMatch[3].trim(), 10); // Cantidad
+        const totalPriceStr = productMatch[4].trim(); // Precio total como string
 
         // Convertir el precio total a número (eliminando el signo de pesos)
         const totalPrice = parseFloat(
-          totalPriceStr.replace(/[$.]/g, "").replace(/,/g, ".")
+          totalPriceStr.replace(/,/g, "").replace(/[$.]/g, "")
         );
 
         // Calcular el precio por unidad
         const precioPorUnidad =
-          cantidad > 0 ? (totalPrice / cantidad).toFixed(6) : 0;
+          cantidad > 0 ? (totalPrice / cantidad).toFixed(4) : 0;
 
         // El %Iva es fijo en este caso
         const iva = "0";
@@ -95,7 +92,7 @@ app.post("/upload", upload.single("pdf"), async (req, res) => {
           iva,
           cantidad,
           `$${precioPorUnidad}`,
-          `$${totalPrice.toFixed(2)}`, // Formatear el precio total
+          `$${totalPrice.toFixed(4)}`, // Formatear el precio total
         ]);
 
         // Aplicar bordes a la fila del producto
